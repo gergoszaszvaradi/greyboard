58afcd78b49d8bbb84263c70cf9d57c14a8103f6
Szasz Varadi Gergo <gergo4_u@yahoo.com>
2021-03-16 18:39:51 +0200

    index.js is now a .ts file

--- public/lib/index.ts
+++ public/lib/index.ts
@@ -1,21 +1,22 @@
 "use strict";
+
 jQuery(() => {
     $("#baord_file").on("change", (e) => {
-        let files = $(e.currentTarget)[0].files;
-        if (files == null)
-            return;
+        let files = ($(e.currentTarget)[0] as HTMLInputElement).files;
+        if(files == null) return;
         let form = new FormData();
         form.append("file", files[0]);
         $(".loading").css("display", "flex").animate({
             opacity: 1
         }, 500);
+
         $.ajax({
             type: "POST",
             url: "/load",
             data: form,
             processData: false,
             contentType: false,
-            success: function (data) {
+            success: function(data){
                 window.location.pathname = `/b/${data}`;
             }
         });
